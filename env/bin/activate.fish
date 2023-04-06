99c57ada95e677f7f1f7d562bf086b194c8c5f83
Nur_013 <user@MacBook-Pro-Nur-00.local>
2023-04-06 16:16:24 +0600

    add venv

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/user/Desktop/Новая папка/env"
+set -gx VIRTUAL_ENV "/Users/user/Desktop/FastAPI/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
