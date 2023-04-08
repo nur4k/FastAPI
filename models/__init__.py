3c240b6ed04fedb2df40a792f9fddf44ccd1a7db
Nur_013 <user@MacBook-Pro-Nur-00.local>
2023-04-08 14:35:12 +0600

    alimbic config

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from .models import Roles, Users
+
+
+__all__ = ['Roles', 'Users']
---
+++ models/models.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from sqlalchemy import JSON, MetaData, Column, Integer, String, TIMESTAMP
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+# Создаем объект Engine для работы с базой данных
+engine = create_engine('postgresql://postgres:@localhost/traiding_db', echo=True)
+
+Base = declarative_base()
+
+metadata = MetaData()
+
+
+class Roles(Base):
+    __tablename__ = 'roles'
+
+    id = Column('id', Integer, primary_key=True)
+    name = Column('name', String, nullable=False)
+    permissions = Column('permissions', JSON)
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column('id', Integer, primary_key=True)
+    email = Column('email', String, nullable=False)
+    username = Column('username', String, nullable=False)
+    password = Column('password', String, nullable=False)
+    registered_at = Column('registered_at', TIMESTAMP, default=datetime.utcnow)
+    roles = relationship(Roles)
+
